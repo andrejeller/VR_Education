e793e9d34d47594fe7aaedaff206a3ed7adc7411
Andre Jeller <enoiseutu@gmail.com>
2019-05-07 11:51:07 -0300

    #01 - Esboco de Layout + Botoes

---
+++ VR_Education/Assets/MiniGames/01_QuizGame/Scripts/VR01_QuizGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class VR01_QuizGame: MonoBehaviour {
+
+    [SerializeField]
+    private GameObject buttonCircle; // 0.09 -> 0.16
+    private float moveTime = 0.3f;
+    private bool canBePressed = true;
+
+    private void Start() {
+        canBePressed = true;
+        Vector3 newPosition = new Vector3(0, 0.16f, 0);
+        buttonCircle.transform.localPosition = newPosition;
+    }
+
+    private void BeingPressed() {
+        canBePressed = false;
+        buttonCircle.transform.DOLocalMoveY(0.09f, 0.3f, false).OnComplete(() => {
+            buttonCircle.transform.DOLocalMoveY(0.16f, 0.3f, false).OnComplete(() => canBePressed = true);
+        });
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        BeingPressed();
+    }
+
+#if UNITY_EDITOR
+    private void OnMouseOver() {
+        if (canBePressed) BeingPressed();
+    }
+#endif
+}
