dffd8e1f94e6b630837fa213057d3cc30f1ee7f5
Andre Jeller <jeller.andre@gmail.com>
2019-05-15 11:28:11 -0300

    #02 Arquitetura - Esperando Line

---
+++ VR_Education/Assets/Games/02_Arquitetura/VR02_Sol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class VR02_Sol : MonoBehaviour {
+
+	public GameObject DirectionalLight;
+	private Vector3 lastRotation;
+
+	private void Start() {
+		lastRotation = transform.rotation.eulerAngles;
+	}
+
+	void Update() {
+		if (lastRotation != transform.rotation.eulerAngles) {
+			DirectionalLight.transform.DORotate(new Vector3(transform.rotation.eulerAngles.z, -90.0f, 0), 0.01f, RotateMode.Fast);
+			lastRotation = transform.rotation.eulerAngles;
+		}
+	}
+}
--- VR_Education/Assets/Scripts/Pointer.cs
+++ VR_Education/Assets/Scripts/Pointer.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pointer : MonoBehaviour {
+public class Pointer:MonoBehaviour {
+
+	public float _distance = 10.0f;
+	public LineRenderer _lineRenderer = null;
+	public LayerMask _everythingMask = 0;
+	public LayerMask _interactableMask = 0;
+
+	private Transform _currentOrigin = null;
 
 	private void Awake() {
 		PlayerEvents.OnControulerSource += UpdateOringin;
 		PlayerEvents.OnTouchPadDown += ProcessTouchpadDown;
+	}
+
+	private void Start() {
+		SetLineColor();
 	}
 
 	private void OnDestroy() {
@@ -14,11 +25,65 @@ public class Pointer : MonoBehaviour {
 		PlayerEvents.OnTouchPadDown -= ProcessTouchpadDown;
 	}
 
-	private void UpdateOringin(OVRInput.Controller controller, GameObject controllerObjects) {
+	private void Update() {
+		Vector3 hitPoint = UpdateLine();
+
+	}
+
+	private Vector3 UpdateLine() {
+		//Create ray
+		RaycastHit hit = CreatedRaycats(_everythingMask);
+
+		//Default end
+		Vector3 endPosition = _currentOrigin.position + (_currentOrigin.forward * _distance);
+
+		//Check hit
+		if (hit.collider != null)
+			endPosition = hit.point;
+
+		//Set position
+		_lineRenderer.SetPosition(0, _currentOrigin.position);
+		_lineRenderer.SetPosition(1, endPosition);
+
+
+		return endPosition;
+	}
+
+	private void UpdateOringin(OVRInput.Controller controller, GameObject controllerObject) {
+		//Set origin of pointer
+		_currentOrigin = controllerObject.transform;
+
+		//is the laser visible?
+		if (controller == OVRInput.Controller.Touchpad) _lineRenderer.enabled = false;
+		else _lineRenderer.enabled = true;
+
+	}
+
+	private RaycastHit CreatedRaycats(int layer) {
+		RaycastHit hit;
+
+		Ray ray = new Ray(_currentOrigin.position, _currentOrigin.forward);
+		Physics.Raycast(ray, out hit, _distance, layer);
+
+		return hit;
+	}
+
+	private void SetLineColor() {
+		if (!_lineRenderer) return;
+
+		Color endColor = Color.white;
+		endColor.a = 0.0f;
+
+		_lineRenderer.endColor = endColor;
 
 	}
 
 	private void ProcessTouchpadDown() {
 
 	}
+
+
+
+
+
 }
