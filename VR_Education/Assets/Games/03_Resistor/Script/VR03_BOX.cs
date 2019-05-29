db1062106881c013045ae3e344b212874a6a1a91
Andre Jeller <jeller.andre@gmail.com>
2019-05-29 09:30:00 -0300

    Merge de Projetos

---
+++ VR_Education/Assets/Games/03_Resistor/Script/VR03_BOX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR03_BOX : MonoBehaviour
+{
+
+// Use this for initialization
+	public GameObject resistPrefab;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerStay(Collider other) {
+		if (other.gameObject.tag == "Hand")
+		{
+			if (other.GetComponent<VR03_GrabAndPin>().grabbed == false && OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+			{
+				Instantiate(resistPrefab, other.gameObject.transform.position, Quaternion.identity);
+			}
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision) {
+	}
+}
---
+++ VR_Education/Assets/Games/03_Resistor/Script/VR03_Pin.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR03_Pin : MonoBehaviour
+{
+	public int pinnedvalue;
+
+	public bool pinned = false;
+
+	private GameObject pinnedObj = null;
+	public GameObject playerHand = null;
+	
+	void Start () {
+	}
+	
+	
+	void Update () {
+		if (pinned)
+		{
+			if (pinnedObj.GetComponent<VR03_Grabable>().grabbed == true)
+			{
+				pinnedvalue = 0;
+				pinned = false;
+				pinnedObj.GetComponent<VR03_Grabable>().pinned = false;
+				pinnedObj = null;
+			}
+			else
+			{
+				pinnedvalue= pinnedObj.GetComponent<VR03_Grabable>().truevalue;
+			}
+		}
+	
+	}
+	private void OnTriggerStay(Collider other)
+	{	
+		if (other.gameObject.tag == "Grabable" && pinned==false && pinnedObj==null && other.GetComponent<VR03_Grabable>().pinned == false && other.GetComponent<VR03_Grabable>().grabbed == false && playerHand.GetComponent<VR03_GrabAndPin>().grabbed == false)
+		{
+			other.GetComponent<VR03_Grabable>().changeParent(gameObject);
+			pinned = true;
+			pinnedObj = other.gameObject;
+		}
+	}
+
+	public int correctPin()
+	{
+		if (pinnedvalue != null)
+		{
+			return (pinnedvalue);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
