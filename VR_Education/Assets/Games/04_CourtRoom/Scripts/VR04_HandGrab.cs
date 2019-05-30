e24e2e97a45f99687cfa74daf2ded23ec9954e55
IsraelMahou <israelplenzi@hotmail.com>
2019-05-30 17:52:52 -0300

    juiz martelo coisa

---
+++ VR_Education/Assets/Games/04_CourtRoom/Scripts/VR04_HandGrab.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class VR04_HandGrab : MonoBehaviour {
+
+	public GameObject grabbedobj;
+	public GameObject teste;
+	public bool canGrab;
+	public bool grabbed;
+	void Start()
+	{
+	
+		canGrab = false;
+		grabbed = false;
+		
+		//teste.gameObject.transform.position = gameObject.transform.position;
+		//teste.gameObject.transform.SetParent(gameObject.transform);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+//		if (Input.GetKey("right"))
+//		{
+//			
+//			transform.Rotate(0,0,5,Space.Self);
+//		}
+//		if (!Input.GetKey("left") && grabbedobj!=null)
+//		{
+//			grabbed = false;
+//			grabbedobj.transform.parent = null;
+//			grabbedobj = null;
+//		}
+		
+		if (!OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger)&& grabbedobj!=null)
+		{	
+			grabbedobj.GetComponent<VR04_Grabable>().nullParent();
+			grabbed = false;
+			grabbedobj = null;
+		}
+	}
+
+
+
+
+
+	private void OnTriggerStay(Collider other)
+	{
+		
+		if (other.gameObject.tag == "Grabable" && grabbed==false)
+		{
+			canGrab = true;
+		}
+
+		if (OVRInput.Get(OVRInput.Button.One))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+		
+//		if (canGrab && Input.GetKey("left") && grabbed==false)
+//		{
+//			other.gameObject.transform.position = gameObject.transform.position;
+//			other.gameObject.transform.SetParent(gameObject.transform);
+//			canGrab = false;
+//			grabbed = true;
+//			grabbedobj = other.gameObject;
+//		}
+
+		if (canGrab && OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+		{
+			other.GetComponent<VR04_Grabable>().changeParent(gameObject);
+			canGrab = false;
+			grabbed = true;
+			grabbedobj = other.gameObject;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (grabbed == false)
+		{
+			canGrab = false;
+		}
+	}
+}
---
+++ VR_Education/Assets/Games/04_CourtRoom/Scripts/VR04_WoodButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class VR04_WoodButton : MonoBehaviour {
+	public AudioClip clip;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Hammer")
+		{
+			AudioSource.PlayClipAtPoint(clip, transform.position);
+		}
+	}
+}
