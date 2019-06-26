689ae1a2a41004effe78b27178c8bf7194cc848d
Andre Jeller <jeller.andre@gmail.com>
2019-06-26 14:34:35 -0300

    #07 - Quase la

---
+++ VR_Education/Assets/Games/07_Quimica/Scripts/VR07_LiquidButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class VR07_LiquidButton : MonoBehaviour, IInteractable {
+
+	public bool holding = false;
+	public VR07_GenerateFakeFluid generator;
+
+
+	public IEnumerable OnTriggerHold() {
+		holding = true;
+		transform.DOLocalMoveZ(0.02f, 0.15f, false);
+		yield return null;
+	}
+
+
+	public IEnumerable OnTriggerRelease() {
+		holding = false;
+		transform.DOLocalMoveZ(0.00f, 0.15f, false);
+		yield return null;
+	}
+
+	private void Update() {
+		if (!holding) return;
+
+		generator.Create();
+
+	}
+
+
+	public IEnumerable OnPointerExit() {
+		throw new System.NotImplementedException();
+	}
+
+	public IEnumerable OnPointerOver() {
+		throw new System.NotImplementedException();
+	}
+
+	public IEnumerable OnTriggerPress() {
+		throw new System.NotImplementedException();
+	}
+	
+
+}
