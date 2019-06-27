d1a3b4da0aa046711d8c9d2208031753774e0637
Andre Jeller <jeller.andre@gmail.com>
2019-06-27 16:45:19 -0300

    #08 ADICIONADOOOO

---
+++ VR_Education/Assets/Games/08_Estagio/scripts/VR08_GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR08_GameManager : MonoBehaviour {
+
+	public static VR08_GameManager instance;
+	private void Awake() {
+		if (instance == null)
+			instance = this;
+		else
+			Destroy(gameObject);
+	}
+
+
+	public Pointer pointer;
+}
