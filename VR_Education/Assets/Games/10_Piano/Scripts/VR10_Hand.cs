d7b87bfce15508ec45165ed7c5956ecb911529a2
IsraelMahou <israelplenzi@hotmail.com>
2019-07-16 17:32:08 -0300

    PIANO 2

---
+++ VR_Education/Assets/Games/10_Piano/Scripts/VR10_Hand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineInternal;
+
+public class VR10_Hand : MonoBehaviour
+{
+	public GameObject trueHand;
+
+	private float initialHeight;
+	// Use this for initialization
+	void Start ()
+	{
+		initialHeight = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float convertYtoXPos = Mathf.Sin(trueHand.transform.rotation.eulerAngles.y * Mathf.Deg2Rad)*5.98f;
+		float convertXtoYPos =  Mathf.Sin(trueHand.transform.rotation.eulerAngles.x * Mathf.Deg2Rad)*2.2f;
+		//transform.position = new Vector3(convertYtoXPos,initialHeight-convertXtoYPos,transform.position.z);
+	}
+}
---
+++ VR_Education/Assets/Games/10_Piano/Scripts/VR10_PianoKeyScript.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class VR10_PianoKeyScript : MonoBehaviour
+{
+	public AudioSource audiosorce;
+	private bool canBePressed = true;
+	private float yy;
+
+	public float semitone_offset=0;
+	// Use this for initialization
+	void Start ()
+	{
+		yy = transform.localPosition.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!canBePressed)
+		{
+			transform.DOMoveX(transform.position.x, 1.3f).OnComplete(() => { canBePressed = true; });
+		}
+	}
+	
+
+	private void OnMouseEnter()
+	{
+		audiosorce.pitch = Mathf.Pow(2f, semitone_offset/12.0f);
+		audiosorce.Play();
+		if(canBePressed)BeingPressed();
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Hand")
+		{
+			audiosorce.pitch = Mathf.Pow(2f, semitone_offset/12.0f);
+			audiosorce.Play();
+			if(canBePressed)BeingPressed();
+		}
+	}
+
+	private void BeingPressed() {
+		canBePressed = false;
+		transform.DOLocalRotate(new Vector3(-8.344001f, 0f, 0f), 0.3f);
+		
+		transform.DOLocalMoveY(yy-0.3f, 0.3f, false).OnComplete(() =>
+		{
+			transform.DOLocalMoveY(yy, 0.3f, false).OnPlay(() =>
+				{
+					transform.DOLocalRotate(new Vector3(0f, 0f, 0f), 0.3f);
+				});
+					
+		});
+	}
+}
