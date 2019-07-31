6e985ca66e30912e0e54642cabc8f673b774bd65
Andre Jeller <jeller.andre@gmail.com>
2019-07-31 16:14:45 -0300

    #14 - Add Basquete
    Falta arrumar a escala/tamanho e tudo relacionado a isso

---
+++ VR_Education/Assets/Games/14_Basquete/Scripts/VR14_Ball.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR14_Ball : MonoBehaviour {
+	
+	void Update () {
+		transform.Rotate(1, 1, 0);
+	}
+}
---
+++ VR_Education/Assets/Games/14_Basquete/Scripts/VR14_GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR14_GameManager:MonoBehaviour {
+	public static VR14_GameManager instance;
+	private void Awake() {
+		if (instance == null)
+			instance = this;
+		else
+			Destroy(gameObject);
+	}
+
+	public GameObject hand, anchor;
+	public GameObject ballPrefab;
+	private GameObject actualBall;
+	public bool HandOnShelf = false;
+	private bool GeneratingBall = false;
+
+	void Update() {
+
+		if (/*HandOnShelf &&*/ !GeneratingBall && (VRInput.TriggerDown() || VRInput.aBotaoTesteDown())) {
+			hand.SetActive(false);
+			GenerateBall();
+		}
+		else if (GeneratingBall && (VRInput.TriggerUp() || VRInput.aBotaoTesteDown())) {
+			hand.SetActive(true);
+			ThrowBall();
+		}
+	}
+
+
+	private void GenerateBall() {
+		GeneratingBall = true;
+		actualBall = Instantiate(ballPrefab, anchor.transform);
+		actualBall.transform.localPosition = new Vector3(0, 0, 1.0f);
+	}
+
+	private void ThrowBall() {
+		actualBall.GetComponent<Rigidbody>().AddForce(anchor.transform.forward * 9000, ForceMode.Acceleration);
+		actualBall.GetComponent<Rigidbody>().useGravity = true;
+		actualBall.transform.parent = null;
+		actualBall = null;
+	}
+
+
+
+}
