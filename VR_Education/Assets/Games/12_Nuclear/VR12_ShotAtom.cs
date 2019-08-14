5735ada0d121020ad51d65c6482f2ffda63d174a
Israel <israelplenzi@hotmail.com>
2019-08-14 17:47:50 -0300

    Nuclear falta interacao final

---
+++ VR_Education/Assets/Games/12_Nuclear/VR12_ShotAtom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR12_ShotAtom : MonoBehaviour {
+    public GameObject objshotatom;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKey(KeyCode.S))
+        {
+            var a = Instantiate(objshotatom);
+            a.GetComponent<Rigidbody>().AddForce(10*transform.forward);
+        }
+	}
+}
