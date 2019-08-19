0120f340beadfd98a6908992fc2b356d538d0268
IsraelMahou <israelplenzi@hotmail.com>
2019-08-18 23:21:32 -0300

    nuclear pronto

---
+++ VR_Education/Assets/Games/12_Nuclear/Scripts/VR12_ShotAtom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR12_ShotAtom : MonoBehaviour {
+    public GameObject objshotatom;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKeyDown(KeyCode.S))
+        {
+            var a = Instantiate(objshotatom, transform.position,transform.rotation);
+            a.GetComponent<Rigidbody>().AddForce(1000*a.transform.forward);
+        }
+	}
+}
