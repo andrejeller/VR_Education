c810ff76cb3d2309b5f6d01f736b54e4c04886fe
andrejeller <jeller.andre@gmail.com>
2019-08-19 16:52:11 -0300

    #18 Iniciando

---
+++ VR_Education/Assets/Games/18_Pescaria/VR18_GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR18_GameManager : MonoBehaviour {
+
+	public static VR18_GameManager instance;
+	private void Awake() {
+		if (instance == null)
+			instance = this;
+		else
+			Destroy(gameObject);
+	}
+
+	public bool inWater = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ VR_Education/Assets/Games/18_Pescaria/VR18_TheFish.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR18_TheFish : MonoBehaviour {
+
+	void Update () {
+		if(!VR18_GameManager.instance.inWater) return;
+	}
+}
