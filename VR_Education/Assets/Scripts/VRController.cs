bd622b7c4be2cf3fa3a8228ca56f0f26c07f09b6
Rafael <rafaelfressato@hotmail.com>
2019-08-22 21:39:53 -0300

    #22 - Iniciado

---
+++ VR_Education/Assets/Scripts/VRController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRController:MonoBehaviour {
+
+
+	public GameObject touch;
+
+	void Update() {
+
+		float x = VRInput.Axes().x;
+		float y = VRInput.Axes().y;
+
+		if (x != 0.0f && y != 0.0f) {
+			x += 1; y += 1;
+			x = Mathf.Lerp(-0.045f, 0.045f, x / 2);
+			//y = Mathf.Lerp(-0.045f, 0.045f, y / 2);
+		}
+	}
+}
