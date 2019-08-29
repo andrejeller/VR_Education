c85d3c8fa4bfd757179019dd139c27468e37e662
andrejeller <jeller.andre@gmail.com>
2019-08-29 19:43:26 -0300

    #01 Correcao de BUGs - #22 Perfeito

--- VR_Education/Assets/Games/01_QuizGame/Scripts/VR01_GameManager.cs
+++ VR_Education/Assets/Games/01_QuizGame/Scripts/VR01_GameManager.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
 public class VR01_GameManager:MonoBehaviour {
 
 	public static VR01_GameManager instance;
@@ -29,22 +34,26 @@ public class VR01_GameManager:MonoBehaviour {
 	private IEnumerator Start() {
 		//DEBUG.dbg.Update("Init Game manager");
 		yield return new WaitForSeconds(0.5f);
-		VR01_XMLManager.instance.LoadQuestions();
+		//VR01_XMLManager.instance.LoadQuestions();
+		LoadXML();
+		yield return new WaitForSeconds(0.8f);
+		DisplayFirstQuestion();
 	}
 
 	private void UpdateQuestionsText(int questionNumber) {
-		ActualCorrectAwnser = VR01_XMLManager.instance.quest[questionNumber].correctAnswer;
-		txt_Question.text = VR01_XMLManager.instance.quest[questionNumber].myQuestion;
-		txt_Awnser0.text = VR01_XMLManager.instance.quest[questionNumber].Answers[0].theAnswerText;
-		txt_Awnser1.text = VR01_XMLManager.instance.quest[questionNumber].Answers[1].theAnswerText;
-		txt_Awnser2.text = VR01_XMLManager.instance.quest[questionNumber].Answers[2].theAnswerText;
-		txt_Awnser3.text = VR01_XMLManager.instance.quest[questionNumber].Answers[3].theAnswerText;
+		ActualCorrectAwnser = QUEST[questionNumber].correctAnswer;
+		txt_Question.text = QUEST[questionNumber].myQuestion;
+		txt_Awnser0.text = QUEST[questionNumber].Answers[0].theAnswerText;
+		txt_Awnser1.text = QUEST[questionNumber].Answers[1].theAnswerText;
+		txt_Awnser2.text = QUEST[questionNumber].Answers[2].theAnswerText;
+		txt_Awnser3.text = QUEST[questionNumber].Answers[3].theAnswerText;
+
 	}
 
 	public void AwnserButtonClicked(int id) {
 		if (!CanRespond) return;
 		CanRespond = false;
-		
+
 		answersPanels.Flash(ActualCorrectAwnser, id);
 	}
 
@@ -53,15 +62,42 @@ public class VR01_GameManager:MonoBehaviour {
 	}
 
 	public void NextQuestion() {
+		ActualQuestion++;
 
-		if (ActualQuestion >= VR01_XMLManager.instance.quest.Count)
-			ActualQuestion = 0;
-		else
-			ActualQuestion++;
+		if (ActualQuestion >= QUEST.Count) ActualQuestion = 0;
 
 		UpdateQuestionsText(ActualQuestion);
 		CanRespond = true;
 	}
 
 	//Translate A,B,C,D to 0,1,2,3
+
+
+	public List<VR01_QuestionToAsk> QUEST { get { return questions.Questions; } set { questions.Questions = value; } }
+	public VR01_Question questions = new VR01_Question();
+	private string fileName = "VR01_Perguntas";
+
+	public void LoadXML() {
+		Debug.Log("[AJS] LOADING AND IMPORTING...");
+		XmlSerializer serializer = new XmlSerializer(typeof(VR01_Question));
+
+		var xmlFile = Resources.Load<TextAsset>(fileName);
+
+		if (xmlFile) {
+
+			questions = serializer.Deserialize(GenerateStreamFromString(xmlFile.text)) as VR01_Question;
+			Debug.Log("[AJS] IMPORTED WITH SUCCESS. :)");
+
+		}
+		else {
+			Debug.LogError("[AJS] IMPORTING FAILED ... File doesen't exist. :/");
+		}
+
+	}
+
+	public static Stream GenerateStreamFromString(string s) {
+		return new MemoryStream(Encoding.UTF8.GetBytes(s));
+	}
+
+
 }
