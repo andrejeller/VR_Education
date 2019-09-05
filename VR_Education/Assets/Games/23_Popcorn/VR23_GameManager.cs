409484779ba0ecf5a6bc70b17c7f1fcea50a90f4
andrejeller <jeller.andre@gmail.com>
2019-09-05 11:31:33 -0300

    #23 Completo - 2 BUGs

---
+++ VR_Education/Assets/Games/23_Popcorn/VR23_GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VR23_GameManager : MonoBehaviour {
+
+	public Transform player;
+	public Transform spawnPosition;
+	public GameObject popcornPrefab;
+
+	private List<GameObject> popcorns = new List<GameObject>();
+
+	void FixedUpdate () {
+
+		if (VRInput.Trigger()) {
+			if (popcorns.Count > 500) popcorns.RemoveAt(0);
+			GameObject a = Instantiate(popcornPrefab, spawnPosition.position, Quaternion.identity);
+			a.GetComponent<Rigidbody>().AddForce((player.position - spawnPosition.position) * 2.8f, ForceMode.Impulse);
+			popcorns.Add(a);
+		}
+
+	}
+}
---
+++ VR_Education/Assets/Games/23_Popcorn/VR23_PopCorn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR23_PopCorn:MonoBehaviour {
+
+	public GameObject milho;
+	public GameObject pipoca;
+
+	float timeInside = 0.0f;
+	private bool boom = false;
+
+	void Start() {
+		pipoca.SetActive(false);
+		milho.SetActive(true);
+		timeInside = 0.0f;
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Fogo" && !boom) {
+			boom = true;
+			pipoca.SetActive(true);
+			milho.SetActive(false);
+			GetComponent<Rigidbody>().AddForce(Vector3.up * 3, ForceMode.Impulse);
+		}
+	}
+
+}
