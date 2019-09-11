25bf0016dcac8fc1af1e7c3ef41e667e379432d6
andrejeller <jeller.andre@gmail.com>
2019-09-11 11:17:28 -0300

    #24 Inicio

---
+++ VR_Education/Assets/Games/24_Math/VR24_Balloon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VR24_Balloon : MonoBehaviour {
+
+	public TextMeshProUGUI number;
+	public Renderer myMaterial;
+
+	private void Start() {
+		Init(3, Color.red);
+	}
+
+	private void Init(int withNumber, Color withColor) {
+		number.text = withNumber.ToString();
+		myMaterial.material.color = withColor;
+	}
+
+}
---
+++ VR_Education/Assets/Games/24_Math/VR24_GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class VR24_GameManager : MonoBehaviour {
+
+
+	[Header("Canvas")]
+	public TextMeshProUGUI MathQuestion;
+	public Sprite on, off;
+	public Image[] buttons = new Image[4];
+
+	[Header("!Canvas")]
+	public GameObject balloonPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
