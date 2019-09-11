7cdb018f784450c5c5c2756f220093a6cdc1eeb1
Israel <israelplenzi@hotmail.com>
2019-09-11 17:29:57 -0300

    Celular visual completo, comecando codigo

---
+++ VR_Education/Assets/Games/27_Celula/VR27_CellComp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR27_CellComp : MonoBehaviour {
+    private bool correct;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!correct) ;
+	}
+}
---
+++ VR_Education/Assets/Games/27_Celula/VR27_botao.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VR27_botao : MonoBehaviour {
+    private bool pareado;
+    private Text txt;
+    
+	// Use this for initialization
+	void Start () {
+        pareado = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pareado)
+        {
+            txt.enabled = true;
+        }
+	}
+}
