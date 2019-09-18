d688974f47720b98433b0be90d410752432ed16a
Nathan <nathan.diogo@gmail.com>
2019-09-18 17:44:39 -0300

    pipa falta deixar bonitinho, mas no geral ta funcional

---
+++ VR_Education/Assets/Games/28_Pipa/VR28_Event.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR28_Event : MonoBehaviour {
+    private float currentTime;
+    public GameObject lamp0;
+    public GameObject lamp1;
+    public GameObject thunderSprite;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+        if(currentTime >= 5f)
+        {
+            lamp0.transform.GetComponent<Light>().intensity = 0.71f;
+            lamp1.transform.GetComponent<Light>().intensity = 0.71f;
+            currentTime = -0.1f;
+        }
+
+        if (currentTime < 0)
+        {
+            thunderSprite.SetActive(true);
+        }
+        else
+        {
+            thunderSprite.SetActive(false);
+        }
+    }
+	
+}
