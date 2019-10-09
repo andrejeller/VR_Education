950b7b2d781b981a1236d439fa002a9f6fdf2d9d
Nathan <nathan.diogo@gmail.com>
2019-10-09 18:15:53 -0300

    Falta uma linha

---
+++ VR_Education/Assets/Games/30_Polemizacao/VR30_BeeMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR30_BeeMove : MonoBehaviour {
+
+    public float flyForce;
+    public float frontFlyForce;
+    public AudioClip canudo;
+    private AudioSource ad;
+
+
+    private float x;
+    private float y;
+    private Vector3 rotateValue;
+    
+
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        ad = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * flyForce);
+            rb.AddForce(transform.forward * frontFlyForce);
+        }
+
+
+        y = Input.GetAxis("Mouse X");
+        x = Input.GetAxis("Mouse Y");
+        rotateValue = new Vector3(x, y * -1, 0);
+        transform.eulerAngles = transform.eulerAngles - rotateValue;
+
+
+    }
+
+    public void canudoPlay()
+    {
+        if (!ad.isPlaying)
+        {
+            ad.PlayOneShot(canudo);
+        }
+    }
+}
---
+++ VR_Education/Assets/Games/30_Polemizacao/VR30_DrinkPolen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR30_DrinkPolen : MonoBehaviour {
+
+    private bool alreadyDrink;
+    private float drinktime = 2f;
+   
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player" && !alreadyDrink)
+        {
+            other.GetComponent<VR30_BeeMove>().canudoPlay();
+            drinktime -= Time.deltaTime;
+            if (drinktime < 0)
+            {
+                alreadyDrink = true;
+                gameObject.SetActive(false);
+                other.GetComponent<AudioSource>().Stop();
+            }
+
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            other.GetComponent<AudioSource>().Stop();
+    }
+}
